157ff12f2370bda8670115480a7ee3db2c9d793b
Rishi Dev <rishidev@rishis-mbp.home>
2020-12-13 15:29:09 -0600

    about us

--- src/Order.tsx
+++ src/Order.tsx
@@ -16,7 +16,7 @@ export interface OrderBody {
 }
 const Order = () => {                             
   const { register, handleSubmit} = useForm();
-
+  const listClass = 'font-semibold w-40 ml-6 text-md tracking-wide'
     const onSubmit = async (data: OrderBody, e: any) => {
         data.state = 'MO'
         try {
@@ -41,7 +41,7 @@ const Order = () => {
   
     return (
       <div className="relative flex items-top justify-center mt-20 bg-white dark:bg-gray-900 sm:items-center sm:pt-0">
-        <div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
+     <div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
           <div className="mt-8 overflow-hidden">
             <div className="grid grid-cols-1 md:grid-cols-2">
               <div className="p-6 mr-2 bg-gray-100 dark:bg-gray-800 sm:rounded-lg">
@@ -49,82 +49,17 @@ const Order = () => {
                   How It Works!
                 </h1>
                 <p className="text-normal text-lg sm:text-2xl font-medium text-gray-600 dark:text-gray-400 mt-2">
-                  Fill in the form to start a conversation
+                  Fill in the form to place your order
                 </p>
-
-                <div className="flex items-center mt-8 text-gray-600 dark:text-gray-400">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="1.5"
-                    viewBox="0 0 24 24"
-                    className="w-8 h-8 text-gray-500"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="1.5"
-                      d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
-                    />
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="1.5"
-                      d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-                    />
-                  </svg>
-                  <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                    Acme Inc, Street, State, Postal Code
-                  </div>
-                </div>
-
                 <div className="flex items-center mt-4 text-gray-600 dark:text-gray-400">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="1.5"
-                    viewBox="0 0 24 24"
-                    className="w-8 h-8 text-gray-500"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="1.5"
-                      d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
-                    />
-                  </svg>
-                  <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                    +44 1234567890
-                  </div>
+                  <ul className="list-disc">
+                    <li className={listClass}>Place order</li>
+                    <li className={listClass}>We will shop</li>
+                    <li className={listClass}>Pay on delivery</li>
+                  </ul>
                 </div>
-
-                <div className="flex items-center mt-2 text-gray-600 dark:text-gray-400">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="1.5"
-                    viewBox="0 0 24 24"
-                    className="w-8 h-8 text-gray-500"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="1.5"
-                      d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-                    />
-                  </svg>
-                  <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                    info@acme.org
-                  </div>
-                </div>
-              </div>
-
+              </div> 
+              
               <form
                 onSubmit={handleSubmit(onSubmit)}
                 className="p-6 flex flex-col justify-center"
@@ -244,7 +179,11 @@ const Order = () => {
                   <label htmlFor="tel" className="hidden">
                     Preferred Grocery Store
                   </label>
-                  <select name="preferredStore" className="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none" ref={register}>
+                  <select
+                    name="preferredStore"
+                    className="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"
+                    ref={register}
+                  >
                     <option value="">Preferred Grocery Store</option>
                     <option value="seema">Seema</option>
                     <option value="desi-bazaar">Desi Bazaar</option>
@@ -252,30 +191,43 @@ const Order = () => {
                 </div>
                 <div className="flex flex-col mt-2">
                   <label htmlFor="tel" className="hidden">
-                    Terms and Conditions 
+                    Terms and Conditions
                   </label>
-                  <div className='flex'>
+                  <div className="flex">
                     <div>
-                  <input type="checkbox" required={true} name="terms" ref={register} />
-                  </div> 
-                <div className='pl-2'>
-                      I agree to GGhar <a className='text-blue-800' href='#'> Terms of Service </a> and <a className='text-blue-800' href='#'> Privacy Policy</a>
-                </div>
+                      <input
+                        type="checkbox"
+                        required={true}
+                        name="terms"
+                        ref={register}
+                      />
+                    </div>
+                    <div className="pl-2">
+                      I agree to GGhar{" "}
+                      <a className="text-blue-800" href="#">
+                        {" "}
+                        Terms of Service{" "}
+                      </a>{" "}
+                      and{" "}
+                      <a className="text-blue-800" href="#">
+                        {" "}
+                        Privacy Policy
+                      </a>
+                    </div>
                   </div>
-                 
                 </div>
 
                 <button
                   type="submit"
                   className="md:w-32 bg-indigo-600 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded-lg mt-3 hover:bg-indigo-500 transition ease-in-out duration-300"
-              >
-                Submit
+                >
+                  Submit
                 </button>
               </form>
+              </div>
             </div>
           </div>
         </div>
-      </div>
     );
 }
 
