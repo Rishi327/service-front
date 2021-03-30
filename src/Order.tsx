8bcde89ec42ec28b6b014d1741879adc95069f84
Rishi Dev <rishidev@Rishis-MBP.attlocal.net>
2021-03-29 23:36:18 -0500

    added something

--- src/Order.tsx
+++ src/Order.tsx
@@ -50,7 +50,7 @@ const Order = () => {
   
     return (
       <div>
-        {orderCount <= 9 ?
+        {orderCount >= 9 ?
       <div className="relative flex items-top justify-center mt-18 bg-white dark:bg-gray-900 sm:items-center sm:pt-0">
         <div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
           <div className="mt-8 overflow-hidden">
